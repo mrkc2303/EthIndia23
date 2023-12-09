753b31133cfa1f6c431043115d98307e4e0b6720
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2023-12-09 22:47:43 +0530

    Added Minor Changes

---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+type VideoPlayerPropsType = {
+  stream: MediaStream | null;
+  isMuted: boolean;
+};
+
+const VideoPlayer = ({ stream, isMuted }: VideoPlayerPropsType) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      console.log(videoRef.current, stream);
+      videoRef.current.srcObject = stream;
+      videoRef.current?.play();
+    }
+  }, [videoRef, stream]);
+
+  return <Video ref={videoRef} muted={true} autoPlay />;
+};
+
+const Video = styled.video`
+  width: 25vw;
+  height: fit-content;
+  border: 2px solid black;
+`;
+
+export default VideoPlayer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,55 @@
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app'
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import { sepolia } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { infuraProvider } from 'wagmi/providers/infura';
+import { useEffect, useState } from 'react';
+const { chains, provider } = configureChains(
+  [sepolia],
+  [
+    infuraProvider({ apiKey: '5524d420b29f4f7a8d8d2f582a0d43f7' }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Connect',
+  projectId: 'connect',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export interface ISpacesComponentProps {
+  children: React.ReactNode;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loadWagmi, setLoadWagmi] = useState(false);
+  const [pgpPrivateKey, setPgpPrivateKey] = useState<string>('');
+
+  useEffect(() => {
+    setLoadWagmi(true);
+  }, []);
+
   return (
-    <SessionProvider
-      session={pageProps.session}
-    >
-      <Component {...pageProps} />
-    </SessionProvider>
+    <WagmiConfig client={wagmiClient}>
+      <SessionProvider
+        session={pageProps.session}
+      >
+        <Component {...pageProps} />
+      </SessionProvider>
+      </WagmiConfig>
   );
 }
